ac20836e408bd548a394ac125ae93ebbf5b5e05b
Gene Gotimer <otherdevopsgene@portinfo.com>
2023-03-04 12:04:18 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# hello-world-github-action
+
+A simple Java application that is built into a container image using GitHub
+Actions.
---
+++ src/main/java/dev/otherdevopsgene/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package dev.otherdevopsgene.helloworld;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class HelloWorld {
+
+  public static void main(final String[] args) {
+    final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a 'on' MMMM d, yyyy'.'");
+    final LocalDateTime now = LocalDateTime.now();
+
+    System.out.println("Hello, world! The current time is " + dtf.format(now));
+  }
+}
